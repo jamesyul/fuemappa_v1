3a9a49e8bebe0a45299422b6bc1f0072c2d808e7
Yul Cardenas <69341405+jamesyul@users.noreply.github.com>
2025-09-05 02:29:56 +0200

    Update navbar

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,6 +1,6 @@
 // --- FICHERO: src/components/common/Navbar.tsx ---
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuthStore } from '../../store/auth.store';
 import SearchBar from './SearchBar';
 import { FaUserCircle } from 'react-icons/fa';
